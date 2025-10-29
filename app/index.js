c0b905926d544e92ca43290ffc8b35e8130be36c
Khoa5108 <khoa5108@example.com>
2025-10-29 12:18:15 +0700

    Xóa toàn bộ file trong repo

--- app/index.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Page() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Xin chào, đây là trang chính!</Text>
-    </View>
-  );
-}
--- index.js
+++
@@ -1,5 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// Đăng ký App.js làm entry point chính
-registerRootComponent(App);
